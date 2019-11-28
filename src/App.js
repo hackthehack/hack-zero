54eb02529c9a548096a3d688bd0dd42bd05bad82
Jonathan Peyper <Jonathon.Peyper@iag.com.au>
2019-11-28 14:42:58 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(awsconfig);
+
 function App() {
   return (
     <div className="App">
@@ -23,4 +29,40 @@ function App() {
   );
 }
 
-export default App;
+const signUpConfig = {
+  header: 'Register for Hack Day!',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password'
+    },
+    {
+      label: 'Given Name',
+      key: 'given_name',
+      required: true,
+      displayOrder: 3,
+      type: 'string'
+    },
+    {
+      label: 'Surname',
+      key: 'family_name',
+      required: true,
+      displayOrder: 4,
+      type: 'string'
+    }
+  ]
+};
+
+export default withAuthenticator(App, { signUpConfig });
